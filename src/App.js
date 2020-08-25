48c6dfb113c6409817a7392bd4e51ea075042e80
1804449@kiit.ac.in <1804449@kiit.ac.in>
2020-08-25 19:11:03 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Pizza from "./components/pizza/Pizza";
+
+function App() {
+  return <Pizza />;
+}
+
+export default App;
